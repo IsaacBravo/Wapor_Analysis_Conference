b8fc664dbfc32f908f51017f298b18c65eb6090e
Isaac Bravo <isaac.bravo@tum.de>
2023-09-16 17:00:05 +0200

    update scripts

--- 02_plot_functions.R
+++ 02_plot_functions.R
@@ -10,7 +10,7 @@ load_libraries <- function(){
   
   # Load packages
   Packages <- c("tidyverse", "tidytext", "purrr", "tidyr", "hrbrthemes", 
-                "marker", "htmltools", "fontawesome")
+                "marker", "htmltools", "fontawesome", "MASS", "patchwork")
   lapply(Packages, library, character.only = TRUE)
   
 }
@@ -95,10 +95,10 @@ group_topic <- function(data){
 group_topic_region <- function(data, level){
   
   result <- data %>%
-    filter(region == tolower(level)) %>% 
-    select(date_time, region, animals, people, causes_binary,
+    dplyr::filter(region == tolower(level)) %>% 
+    dplyr::select(date_time, region, animals, people, causes_binary,
            consequences_binary, solutions) %>%
-    mutate(category = case_when(
+    dplyr::mutate(category = case_when(
       animals == "animals" ~ "Animals",
       causes_binary == "Yes" ~ "Causes of CC",
       consequences_binary == "Yes" ~ "Consequences of CC",
@@ -107,15 +107,15 @@ group_topic_region <- function(data, level){
       solutions == "Protests" ~ "Protests",
       TRUE ~ NA_character_
     )) %>%
-    filter(!is.na(category)) %>%
-    mutate(year = lubridate::year(date_time),
+    dplyr::filter(!is.na(category)) %>%
+    dplyr::mutate(year = lubridate::year(date_time),
            region = str_to_title(region)) %>%
-    group_by(year, category) %>%
-    summarize(total = n()) %>%
-    mutate(percent = round((total/sum(total)), 3), label = paste0(percent*100, "%")) %>%
+    dplyr::group_by(year, category) %>%
+    dplyr::summarize(total = n()) %>%
+    dplyr::mutate(percent = round((total/sum(total)), 3), label = paste0(percent*100, "%")) %>%
     ungroup() %>% 
-    mutate(region = str_to_title(level)) %>% 
-    select(region, everything())
+    dplyr::mutate(region = str_to_title(level)) %>% 
+    dplyr::select(region, everything())
   
   return(result)
 }
@@ -124,14 +124,14 @@ plot_topic_region <- function(data, legend_label){
   
   data %>%
     ggplot(aes(x = "", y = percent, fill = as.factor(category))) +
-    ggchicklet::geom_chicklet(radius = grid::unit(6, "pt")) +
+    ggchicklet::geom_chicklet(radius = grid::unit(6, "pt"), width = .5) +
     geom_text(aes(label = label),
               size = 3.0, fontface = "bold",
               position = position_stack(vjust = .5, reverse = TRUE)) +
     facet_wrap(~ year, ncol = 1, strip.position = "left") +
     coord_flip() +
     labs(
-      title = paste0("Topic Distribution in ", legend_label, " Region by Year"),
+      title = paste0("Topic Distribution in ", legend_label, " region by year"),
       x = "",
       y = "",
       fill = "Topics:"
@@ -147,6 +147,7 @@ plot_topic_region <- function(data, legend_label){
           plot.background = element_rect(fill = "white", colour = "white", size = 3),
           plot.margin = unit(c(7, 10, 3, 10), units = "mm"),
           panel.background = element_rect(fill = "white", colour = "NA"),
+          panel.spacing=unit(0, "lines"),
           strip.text = element_text(family = "Roboto Condensed", colour = "black", 
                                     face = "bold", size = 11, angle = -90),
           legend.title = element_text(family = "Lora", size = 12,
@@ -184,7 +185,7 @@ plot_valence_region <- function(data, legend_label){
   
   data %>%
     ggplot(aes(x = year, y = percent, fill = top_valence)) +
-    geom_bar(position="fill", stat="identity", width = 1) +
+    geom_bar(position="fill", stat="identity", width = .5) +
     geom_text(aes(label = label),
               size = 2.5, fontface = "bold",
               position = position_stack(vjust = .5)) +
@@ -264,8 +265,68 @@ export_png <- function(x, legend_label){
   
 }
 
+export_prevalence_png <- function(x, legend_label){
+  
+  ggsave(filename = paste0("./viz/Prevalence_by_Region/", legend_label, ".png"),
+         plot = x,
+         pointsize = 24, 
+         width = 16 ,
+         height = 10,
+         scale = 0.5,
+         dpi = 800)
+  
+}
 
+#------------------------ Plot Regression Coef --------------------------------#
 
+plot_coef <- function(model, level){
+  coefplot::coefplot(model,
+                     # title = "Regression Estimates",
+                     xlab = "Value",
+                     ylab = "Coefficient",
+                     innerCI = 1,
+                     outerCI = 2,
+                     color = "lightblue",
+                     shape = 16,
+                     cex = 0.8,
+                     textAngle = 0,
+                     numberAngle = 0,
+                     zeroColor = "red") +
+    theme_minimal() +
+    labs(subtitle = paste0("Impact of ", level, " on Engagement")) +
+    geom_text(aes(label = round(model$coefficients, 2  )), nudge_x = 0.2, nudge_y = 0.01, fontface = "bold") + 
+    theme(legend.position = "top",
+          plot.title = element_blank(
+            # size = 18,
+            # face = "bold"
+          ),
+          plot.subtitle = element_text(hjust = 0., family = "Lora",
+                                       size = 14, colour = "black"),
+          plot.caption = element_text(family = "Roboto Condensed", colour = "black", face = "bold"),
+          plot.background = element_rect(fill = "white", colour = "white", size = 3),
+          plot.margin = unit(c(7, 10, 3, 10), units = "mm"),
+          panel.background = element_rect(fill = "white", colour = "NA"),
+          strip.text = element_text(family = "Roboto Condensed", colour = "black", face = "bold", size = 11),
+          
+          legend.title = element_text(family = "Lora", size = 12, 
+                                      hjust = 0.5, face = "bold", colour = "black"),
+          legend.key = element_blank(),
+          legend.background = element_rect(fill = "white", colour = "white"),
+          legend.key.width = unit(10, "mm"),
+          legend.key.size = unit(.1, "mm"),
+          legend.text = element_text(family = "Roboto Condensed", hjust = 0.5, size = 12,
+                                     colour = "black"),
+          legend.direction = "horizontal",
+          panel.grid.minor = ggplot2::element_blank(),
+          panel.grid.major.y = ggplot2::element_line(color="#DFDFDF"),
+          panel.grid.major.x = ggplot2::element_blank(),
+          axis.line = ggplot2::element_line(color="black", linewidth = 1),
+          axis.ticks = element_blank(),
+          axis.text.y = element_text(family = "Roboto Condensed", hjust = 0.5, size = 10,
+                                     colour = "black"),
+          axis.title = element_blank())
+  
+}
 
 
 
---
+++ 04_regression_models.R
@@ -0,0 +1,94 @@
+#------------------------------------------------------------------------------
+# Topic: Explore Twitter Data - Climate Change Post (2019 - 2023)
+# Created by: Isaac Bravo
+# Source: https://twitter.com
+#------------------------------------------------------------------------------
+
+#------------------------------- Load Libraries -------------------------------#
+source("02_plot_functions.R")
+
+load_libraries()
+
+#------------------------------- Import Data ----------------------------------#
+
+df_labels <- readRDS("./data/df_labels.RDS") 
+
+#------------------------------- Prepare Data ---------------------------------#
+
+df_full_reg <- df_labels %>% 
+  dplyr::mutate_at(vars(12:21), as.factor) %>% 
+  dplyr::select(-lang, -author_id, -text) %>%
+  dplyr::mutate(engagement = retweet_count + like_count)
+
+#---------------------- Run Negative Binomnial Regression ---------------------#
+
+# Topic: Animals
+m1 <- glm.nb(engagement ~ animals + region, data = df_full_reg)
+
+# Topic: Causes Climate Change (Binary)
+m2 <- glm.nb(engagement ~ causes_binary + region, data = df_full_reg)
+
+# Topic: Causes Climate Change (Multiple)
+m2.2 <- glm.nb(engagement ~ causes_multiple + region, data = df_full_reg)
+
+# Topic: Consequences Climate Change (Binary)
+m3 <- glm.nb(engagement ~ consequences_binary + region, data = df_full_reg)
+
+# Topic: Consequences Climate Change (Multiple)
+m3.2 <- glm.nb(engagement ~ consequences_multiple + region, data = df_full_reg)
+
+# Topic: People
+m4 <- glm.nb(engagement ~ people + region, data = df_full_reg)
+
+
+
+#-------------------------- Save Regression Models ----------------------------#
+model_list <- list()
+
+model_list[["m1_animals"]] <- m1
+model_list[["m2_causes_bin"]] <- m2
+model_list[["m2.2_causes_mul"]] <- m2.2
+model_list[["m3_consequences_bin"]] <- m3
+model_list[["m3.2_consequences_mul"]] <- m3.2
+model_list[["m4_people"]] <- m4
+
+saveRDS(model_list, "model_results.RDS")
+
+#-------------------------- Plot Regression Models ----------------------------#  
+
+plt1 <- plot_coef(m1, "Animals")
+plt2 <- plot_coef(m2, "Causes of Climate Change")
+plt3 <- plot_coef(m3, "Consequences of Climate Change")
+plt4 <- plot_coef(m4, "People")
+
+plt5 <- plot_coef(nb_engagement_solutions_1, "Protests")
+plt6 <- plot_coef(nb_engagement_solutions_2, "Climate Summits")
+plt7 <- plot_coef(nb_engagement_solutions_3, "Politics")
+
+plt5 + plt6 + plt7
+
+plt_total <- plt1 + plt2 + plt3 + plt4
+
+plt_total
+
+ggsave(filename = "./plt_total.png",
+       plot = plt_total,
+       pointsize = 24, 
+       width = 28,
+       height = 18,
+       scale = 0.5,
+       dpi = 800)
+
+#------------------------------- Clean Workplace ------------------------------#  
+
+# List all objects in the workspace
+all_objects <- ls()
+
+# Remove the selected objects
+rm(list = all_objects)
+
+
+
+
+
+
